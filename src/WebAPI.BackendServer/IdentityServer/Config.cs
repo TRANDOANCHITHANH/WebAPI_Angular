97674d2b7cc7956cd72a474205a59bf97baefd70
TRANDOANCHITHANH <tranchithanh0404@gmail.com>
2025-02-28 23:57:25 +0700

    Identity Server

---
+++ src/WebAPI.BackendServer/IdentityServer/Config.cs
@@ -0,0 +1,93 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace WebAPI.BackendServer.IdentityServer
+{
+	public class Config
+	{
+		public static IEnumerable<IdentityResource> Ids =>
+			new List<IdentityResource>
+			{
+				new IdentityResources.OpenId(),
+				new IdentityResources.Profile()
+			};
+		public static IEnumerable<ApiResource> Apis =>
+			new List<ApiResource>
+			{
+				new ApiResource("api.webapi", "Web API")
+			};
+		public static IEnumerable<Client> Clients =>
+			new Client[] {
+				new Client{
+					ClientId = "webportal",
+					ClientSecrets = { new Secret("secret".Sha256()) },
+					AllowedGrantTypes = GrantTypes.Code,
+					RequireConsent = false,
+					RequirePkce = true,
+					AllowOfflineAccess = true,
+					RedirectUris = { "https://localhost:7196/sigin-oidc" },
+					PostLogoutRedirectUris = { "https://localhost:7196/signout-callback-oidc" },
+					AllowedScopes = {
+					 IdentityServerConstants.StandardScopes.OpenId,
+					 IdentityServerConstants.StandardScopes.Profile,
+					 IdentityServerConstants.StandardScopes.OfflineAccess,
+					 "api.webapi"
+					}
+				},
+					new Client{
+						ClientId = "swagger",
+						ClientName = "Swagger Client",
+
+						AllowedGrantTypes = GrantTypes.Implicit,
+						AllowAccessTokensViaBrowser = true,
+						RequireConsent = false,
+
+						RedirectUris = { "https://localhost:7196/swagger/oauth2-redirect.html" },
+						PostLogoutRedirectUris = { "https://localhost:7196/swagger/oauth2-redirect.html" },
+						AllowedCorsOrigins = { "https://localhost:7196" },
+
+						AllowedScopes = new List<string>
+						{
+							IdentityServerConstants.StandardScopes.OpenId,
+							IdentityServerConstants.StandardScopes.Profile,
+							"api.webapi"
+						}
+					},
+					new Client
+					{
+						ClientName = "Angular Admin",
+						ClientId = "angular_admin",
+						AllowedGrantTypes = GrantTypes.Code,
+						AccessTokenType = AccessTokenType.Jwt,
+						RequireConsent = false,
+
+						RequireClientSecret = false,
+						RequirePkce = true,
+						AllowAccessTokensViaBrowser = true,
+						RedirectUris = new List<string>
+						{
+							"https://localhost:4200",
+							"https://localhost:4200/signin-callback",
+							"https://localhost:4200/assets/silent-callback.html"
+						},
+						PostLogoutRedirectUris = new List<string>
+						{
+							"https://localhost:4200/unauthorized",
+							"https://localhost:4200/signout-callback",
+							"https://localhost:4200"
+						},
+						AllowedCorsOrigins = new List<string>
+						{
+							"https://localhost:4200"
+						},
+						AllowedScopes = new List<string>
+						{
+							IdentityServerConstants.StandardScopes.OpenId,
+							IdentityServerConstants.StandardScopes.Profile,
+							"api.webapi"
+						},
+					}
+				};
+	};
+}
+
--- src/WebAPI.BackendServer/Program.cs
+++ src/WebAPI.BackendServer/Program.cs
@@ -1,15 +1,18 @@
 using FluentValidation;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using WebAPI.BackendServer.Data;
 using WebAPI.BackendServer.Data.Entities;
+using WebAPI.BackendServer.IdentityServer;
+using WebAPI.BackendServer.Services;
 using WebAPI.ViewModels.Systems;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 builder.Services.AddValidatorsFromAssemblyContaining<RoleVMValidator>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,7 +23,18 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddIdentity<User, IdentityRole>()
 	.AddEntityFrameworkStores<ApplicationDbContext>()
 	.AddDefaultTokenProviders();
-
+builder.Services.AddIdentityServer(options =>
+{
+	options.Events.RaiseErrorEvents = true;
+	options.Events.RaiseInformationEvents = true;
+	options.Events.RaiseFailureEvents = true;
+	options.Events.RaiseSuccessEvents = true;
+}).AddInMemoryApiResources(Config.Apis)
+	.AddInMemoryClients(Config.Clients)
+	.AddInMemoryIdentityResources(Config.Ids)
+	.AddAspNetIdentity<User>();
+builder.Services.AddTransient<IEmailSender, EmailSenderService>();
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,11 +43,17 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
-
+app.UseIdentityServer();
 app.UseHttpsRedirection();
-
+app.MapDefaultControllerRoute();
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapControllers();
-
+app.MapRazorPages();
+/*app.UseEndpoints(endpoints =>
+{
+	endpoints.MapControllers();
+	endpoints.MapRazorPages();
+});*/
 app.Run();
